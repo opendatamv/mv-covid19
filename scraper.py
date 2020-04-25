08c4c37d5454d12236092e4edd0f02708516d8a9
kudanai <kudanai@gmail.com>
2020-04-25 13:40:45 +0800

    update data

--- scraper.py
+++ scraper.py
@@ -6,6 +6,13 @@ from csv import DictWriter
 import re
 import json
 
+
+def parse_last_update(page):
+    soup = BeautifulSoup(page.text, features="html.parser")
+    update_tag = soup.find(lambda tag:tag.name=="div" and tag.text.startswith("Last Updated Date"))
+    return update_tag.text
+
+
 def parse_edges(page):
     try:
         pattern = re.search(
@@ -34,7 +41,7 @@ def parse_nodes(page):
 def write_edges(edges):
     if edges:
         with open("edges_official.csv", "w") as f:
-            writer = DictWriter(f, edges[0].keys())
+            writer = DictWriter(f, ['to', 'from', 'dashes'] )
             writer.writeheader()
             writer.writerows(edges)
 
@@ -53,6 +60,19 @@ if __name__ == "__main__":
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
                 }
         )
+    if doc.status_code == 200:                  # note: requests doesn't necessary mean HTTP200 here
+        updated_on = parse_last_update(doc)
+        print(f"Fetched Dashboard: {updated_on}")
+        
+        # process nodes
+        nodes = parse_nodes(doc)
+        if nodes:
+            print(f"Writing {len(nodes)} Nodes to file")
+            write_nodes(nodes)
 
-    write_nodes(parse_nodes(doc))
-    write_edges(parse_edges(doc))
+        edges = parse_edges(doc)
+        if edges:
+            print(f"Writing {len(edges)} Edges to file")
+            write_edges(edges)
+    else:
+        print("Error: Response returned unsuccessful")
